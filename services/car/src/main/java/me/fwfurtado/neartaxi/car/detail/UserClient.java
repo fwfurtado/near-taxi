45e63550b2206df8a96d8f668aae622442e5fce5
Fernando <feh.wilinando@gmail.com>
2019-11-01 10:16:59 -0300

    Hystrix dashboard

--- services/car/src/main/java/me/fwfurtado/neartaxi/car/detail/UserClient.java
+++ services/car/src/main/java/me/fwfurtado/neartaxi/car/detail/UserClient.java
@@ -4,11 +4,14 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.fwfurtado.neartaxi.car.detail.UserClient.UserClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "users")
+@FeignClient(name = "users", fallback = UserClientFallback.class)
 public interface UserClient {
 
     @GetMapping("{id}")
@@ -20,5 +23,13 @@ public interface UserClient {
     class OwnerView {
 
         private String name;
+    }
+
+    @Component
+    class UserClientFallback implements UserClient {
+        @Override
+        public OwnerView findOwnerById(Long id) {
+            return new OwnerView("Unknown");
+        }
     }
 }
