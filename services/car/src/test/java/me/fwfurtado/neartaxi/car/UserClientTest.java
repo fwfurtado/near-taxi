b039b38fbb37f20c892beb75ff78906fcf9b8bb8
Fernando <feh.wilinando@gmail.com>
2019-11-04 14:56:28 -0300

    Fix test and groupid

---
+++ services/car/src/test/java/me/fwfurtado/neartaxi/car/UserClientTest.java
@@ -0,0 +1,31 @@
+package me.fwfurtado.neartaxi.car;
+
+import me.fwfurtado.neartaxi.car.detail.UserClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureStubRunner(stubsMode = StubsMode.LOCAL, ids = "me.fwfurtado.neartaxi:user:+:8090")
+@AutoConfigureTestDatabase
+public class UserClientTest {
+
+    @Autowired
+    private UserClient client;
+
+    @Test
+    public void shouldGetAUser() {
+
+        var owner = client.findOwnerById(1L);
+
+        System.out.println(owner.getId());
+        System.out.println(owner.getName());
+    }
+
+}
